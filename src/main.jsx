e9aa6db2af285417305f88cbfdafe9c84c604977
HtetPaingLinn <htetpainglinn2822004@gmail.com>
2024-04-26 00:43:17 +0630

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,30 @@
-import { createRoot } from 'react-dom/client';
+import { createElement } from "react";
+import { createRoot } from "react-dom/client";
 const rootElement = document.querySelector("#root");
+
+const tasks = [
+  "Read Js Book",
+  "Eat Less",
+  "Sleep Well",
+  "Learn N3 and N2 Japanese",
+];
+const x = tasks.map((el) => console.log(el));
 const root = createRoot(rootElement);
-root.render()
+root.render(
+  createElement(
+    "div",
+    null,
+    createElement("h1", null, "Todo App"),
+    createElement(
+      "Form",
+      null,
+      createElement("input", null, null),
+      createElement("button", null, "Add New")
+    ),
+    createElement(
+      "ul",
+      null,
+      tasks.map((task,index) => createElement("li", {key: index}, task))
+    )
+  )
+);
