ef7043b46d63066a8d29c0fbf3ad0326be286ecc
HtetPaingLinn <htetpainglinn2822004@gmail.com>
2024-05-04 04:02:17 +0630

    Final Commid

--- src/EmprtList.jsx
+++ src/EmprtList.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import image from './empty.svg'
 
 function EmprtList() {
   return (
     
         <div className="hidden last:flex flex-col items-center py-10">
-          <img width={200} className="mb-5" src="./src/empty.svg" alt="note" />
+          <img width={200} className="mb-5" src={image} alt="note" />
           <p className="text-sm">There is no list.</p>
         
       </div>
